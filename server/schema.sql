c33786fd6e18582d2d40148ef435058e69a633c2
Luiz Gabriel Rebelatto <lbrebelatto@gmail.com>
2026-06-19 08:21:59 -0300

    database created

---
+++ server/schema.sql
@@ -0,0 +1,34 @@
+CREATE EXTENSION IF NOT EXISTS pgcrypto;
+
+CREATE TABLE IF NOT EXISTS event_statuses (
+    id TEXT PRIMARY KEY,
+    label
+)
+
+CREATE TABLE IF NOT EXISTS events (
+  id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+  name TEXT NOT NULL,
+  date DATE NOT NULL,
+  status_id TEXT NOT NULL REFERENCES event_statuses(id),
+  guests_limit INTEGER NOT NULL,
+  photos_per_guest INTEGER NOT NULL,
+  created_at TIMESTAMP NOT NULL DEFAULT NOW()
+);
+
+CREATE TABLE IF NOT KEY EXISTS guests (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    event_id UUID NOT NULL REFERENCES events(id) ON DELETE CASCADE,
+    name TEXT NOT NULL,
+    email TEXT NOT NULL,
+    status TEXT NOT NULL CHECK (status IN ('invited', 'joined')),
+    photos_count INTEGER NOT NULL DEFAULT 0,
+    joined_at TIMESTAMP
+)
+
+CREATE TABLE IF NOT EXISTS photos (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    event_id UUID NOT NULL REFERENCES events(id) ON DELETE CASCADE,
+    guest_name TEXT NOT NULL,
+    url TEXT NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT NOW()
+)
