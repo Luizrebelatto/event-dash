3981b7e2c282a8ad679437d157255cbd888e5660
Luiz Gabriel Rebelatto <lbrebelatto@gmail.com>
2026-06-21 10:01:33 -0300

    config tanstack query

---
+++ src/app/providers.tsx
@@ -0,0 +1,18 @@
+import type { ReactNode } from "react";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+import { queryClient } from "../service/queryClient"
+
+type ProvidersProps = {
+    children: ReactNode
+}
+
+export function Providers({ children }: ProvidersProps) {
+    return (
+        <QueryClientProvider client={queryClient}>
+            {children}
+            <ReactQueryDevtools initialIsOpen={false}/>
+        </QueryClientProvider>
+    )
+}
